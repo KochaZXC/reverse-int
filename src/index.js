3c895ab0491a792931fbf6cd1f1fe1ed4cbd80ab
KochaZXC <rubbickmage@gmail.com>
2023-08-06 19:33:03 +0300

    feat: first try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numToStroke = String(n);
+    let reverseNum = '';
+
+    for (let i = numToStroke.length - 1; i >= 0; i--) {
+        if(isNaN(+numToStroke[i])) {
+            continue;
+        } else {
+            reverseNum += numToStroke[i];
+        }
+    }
+    return +reverseNum;
 }
+
